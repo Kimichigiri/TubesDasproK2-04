e436842fe09b3bf9087bb9c583919c7cc4d64682
Kimic <130971242+Kimichigiri@users.noreply.github.com>
2023-05-01 12:32:11 +0800

    Benerin laporan jin ada lupa ngereset i = 0

--- F09.py
+++ F09.py
@@ -15,10 +15,10 @@ def laporanjin():
         if globalvars.user[i][2] == "jin_pembangun":    
             appends(arrayjin, globalvars.user[i][0])
         i += 1
-    i = 0   # i kembali 0
 
     jumlah_jin = countJin(globalvars.user, "jin_pembangun")     # Menghitung jumlah jin
     for h in range (jumlah_jin):                # Menghitung frekuensi candi yang dibuat oleh jin tertentu secara traversal
+        i = 0       # i kembali 0
         count_candi = 0
         while globalvars.candi[i] != "\0":         # O(N^2)
             if globalvars.candi[i][1] == arrayjin[h]:
@@ -27,10 +27,8 @@ def laporanjin():
             i += 1
         appends(candi_dibuat, count_candi)  
 
-
     sortLex(arrayjin, candi_dibuat, jumlah_jin)     # Sort secara leksikografis
 
-    
     print()
     print(f"> Total Jin: {findMark(globalvars.user) - 2}")                          # Total jumlah jin
     print(f"> Total Jin Pengumpul: {countJin(globalvars.user, 'jin_pengumpul')}")   # Total jin pengumpul
